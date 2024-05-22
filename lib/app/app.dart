422f5d92550110f2e09c3ee07fdd84b9b27f17bb
Carlos Parra <altrya-dev@hotmail.com>
2024-05-22 11:18:36 +0200

    remove b.l. absolutely

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:taller_29_mayo_front/app/view/auth/auth_controller.dart';
 
 import 'view/auth/login/login_view.dart';
 
@@ -8,18 +6,13 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<AuthController>(create: (_) => AuthController()),
-      ],
-      child: MaterialApp(
-        title: 'My TODO App',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
-          useMaterial3: true,
-        ),
-        home: const LoginView(),
+    return MaterialApp(
+      title: 'My TODO App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: true,
       ),
+      home: const LoginView(),
     );
   }
 }
--- lib/app/view/auth/register/register_view.dart
+++ lib/app/view/auth/register/register_view.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:taller_29_mayo_front/app/reutilizables/my_button.dart';
 import 'package:taller_29_mayo_front/app/reutilizables/my_text_field.dart';
-import 'package:taller_29_mayo_front/app/view/auth/auth_controller.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -14,53 +12,45 @@ class RegisterView extends StatefulWidget {
 class _RegisterViewState extends State<RegisterView> {
   @override
   Widget build(BuildContext context) {
-    return Consumer<AuthController>(
-      builder: (_, provider, __) => Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          backgroundColor: Colors.blueAccent,
-          title: const Text('Registro'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 7.0),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'Únete a nuestro gran servicio para gestionar tus tareas',
-                  style: TextStyle(fontSize: 20),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 40),
-                MyTextField(
-                  labelText: 'Usuario',
-                  controller: provider.registerUserName,
-                ),
-                const SizedBox(height: 20),
-                MyTextField(
-                  labelText: 'Contraseña',
-                  passwordMode: true,
-                  controller: provider.registerPassword,
-                ),
-                const SizedBox(height: 20),
-                MyTextField(
-                  labelText: 'Repite la contraseña',
-                  passwordMode: true,
-                  controller: provider.registerConfirmPassword,
-                ),
-                const SizedBox(height: 30),
-                MyButton(
-                  label: 'Registro',
-                  onPressed: () async {
-                    bool success = await provider.registerUser(context);
-                    if (success) {
-                      Navigator.pop(context);
-                    }
-                  },
-                ),
-              ],
-            ),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: const Text('Registro'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 7.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Únete a nuestro gran servicio para gestionar tus tareas',
+                style: TextStyle(fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 40),
+              MyTextField(
+                labelText: 'Usuario',
+              ),
+              const SizedBox(height: 20),
+              MyTextField(
+                labelText: 'Contraseña',
+                passwordMode: true,
+              ),
+              const SizedBox(height: 20),
+              MyTextField(
+                labelText: 'Repite la contraseña',
+                passwordMode: true,
+              ),
+              const SizedBox(height: 30),
+              MyButton(
+                label: 'Registro',
+                onPressed: () async {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
           ),
         ),
       ),
